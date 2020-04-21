c02d52fc51234b8487e6dd59fcbf20381cdd3892
yaoyubj <yaoyubj@cn.ibm.com>
2020-04-21 17:54:10 +0800

    updated the init app name for spring security 1

--- security1/src/main/java/com/ibm/esw/leo/spring/security/SpringbootSecurityApplication.java
+++ security1/src/main/java/com/ibm/esw/leo/spring/security/SpringbootSecurityApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootSecurityApplication2 {
+public class SpringbootSecurityApplication {
     public static void main(String[] args) {
-        SpringApplication.run( SpringbootSecurityApplication2.class, args );
+        SpringApplication.run( SpringbootSecurityApplication.class, args );
     }
 }
